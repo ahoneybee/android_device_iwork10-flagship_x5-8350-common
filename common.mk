0308812f197d26dc4fc6244c124a0f2303dd3422
Captain Throwback <captainthrowback@hotmail.com>
2022-05-21 10:33:03 -0400

    sm8250: devices don't use project quotas and casefolding
    Fixes Format Data issue in TWRP
    
    Change-Id: I429db1534970077b779e176dccc418b8fb5df50d

--- common.mk
+++ common.mk
@@ -1,9 +1,6 @@
 # Inherit from common AOSP config
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
-# Enable project quotas and casefolding for emulated storage without sdcardfs
-$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
-
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
